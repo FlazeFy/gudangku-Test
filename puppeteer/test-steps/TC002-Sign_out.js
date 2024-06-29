f98b8a4a6fd86fdac893016cb46140d0b6f30258
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-06-29 09:04:01 +0700

    [Puppeteer TEST] Test steps TC-002

---
+++ puppeteer/test-steps/TC002-Sign_out.js
@@ -0,0 +1,71 @@
+const puppeteer = require('puppeteer');
+const { add_firestore } = require('../../audit/firebase/command')
+const BASEURL = 'http://127.0.0.1:8000'
+
+puppeteer.launch({ headless: false }).then(async (browser) => {
+    const page = await browser.newPage()
+    const date = new Date()
+    await page.setViewport({ width: 1920, height: 1080 })
+
+    try {
+        // Test Data
+        const username = 'flazefy'
+        const password = 'nopass123'
+
+        // Test Step 1 : Pengguna telah masuk kedalam aplikasi
+        await page.goto(`${BASEURL}/login`)
+
+        await page.locator('input#username').fill(username)
+        await page.locator('input#password').fill(password)
+        await page.locator('a#submit_btn').click()
+        await page.waitForNavigation()
+
+        // Test Step 2 : Pengguna menekan menu My Inventory
+        await page.locator('button#nav_profile_btn').click()
+        await page.waitForNavigation()
+        await page.screenshot({
+            path: `TC-002-Step 2-Result on ${date}.png`,
+        })
+
+        // Test Step 3 : Pengguna menekan tombol sign out
+        await page.locator('a#sign_out_btn').click()
+        await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 3000)))
+        await page.screenshot({ path: `TC-002-Step 3-Result on ${date}.png` })
+
+        // Test Step 4 : Pada pop up validasi, pengguna menekan tombol Yes, Sign Out
+        await page.waitForFunction(
+            () => document.querySelector('div#modalSignOut').classList.contains('show'),
+            { timeout: 3000 }
+        );
+
+        await page.locator('a#validation_sign_out_btn').click()
+        await page.waitForNavigation()
+        await page.screenshot({ path: `TC-002-Step 4-Result on ${date}.png` })
+
+        // Audit
+        let data = {
+            pic: 'Leonardho R Sitanggang',
+            tc_id: 'TC-002',
+            tc_name: 'Sign Out',
+            tc_desc: 'Validate Test Steps for sign out',
+            tc_data: {
+                username: username,
+                password: password,
+            },
+            tc_evidence_name: [`TC-002-Step 2-Result on ${date}.png`,`TC-002-Step 3-Result on ${date}.png`,`TC-002-Step 4-Result on ${date}.png`],
+            tested_at: date,
+            tc_base_url: BASEURL
+        };
+        await add_firestore(data, 'test_audit_gudangku')
+
+        await page.evaluate(() => {
+            alert(`TC-002 Sign Out Test Success`)
+        }, [])
+    } catch (error) {
+        console.error('Error occurred:', error.message)
+    } finally {
+        await browser.close()
+    }
+}).catch((error) => {
+    console.error('Failed to launch browser:', error)
+});
